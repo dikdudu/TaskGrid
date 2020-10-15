6385b437998407d00e5a7813e3786edd946efd7b
unknown <dikdudu@hotmail.com>
2020-10-15 10:44:04 -0300

    primeiro commit

---
+++ src/components/TaskGrid.vue
@@ -0,0 +1,19 @@
+<template>
+  <dir class="tasks">
+      <template>
+          <div v-for="task in tasks" :key="task.name">{{task.name}}</div>
+      </template>
+  </dir>
+</template>
+
+<script>
+export default {
+    props: {
+        tasks: {type: Array, require: true}
+    }
+}
+</script>
+
+<style>
+
+</style>
